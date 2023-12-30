17528b5d1b5a3f16881b4d4192c5fa7a071134d5
Alex <wikialex02@gmail.com>
2023-12-30 22:39:17 +0100

    test with nested messages

--- task_planner.cpp
+++ task_planner.cpp
@@ -1,25 +1,31 @@
 #include "ros/ros.h"
 #include "ur5_lego/Vectorize.h"
 #include <cstdlib>
+#include <vector>
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "task_planner");
-	if (argc != 2) {
-		ROS_INFO("usage: task_planner X");
+	if (argc != 3) {
+		ROS_INFO("usage: task_planner X DIM");
 		return 1;
 	}
 
 	long int x = atoll(argv[1]);
+	long int dim = atoll(argv[2]);
 
 	ros::NodeHandle n;
 	ros::ServiceClient service_example = n.serviceClient<ur5_lego::Vectorize>("vectorize_example");
 
 	ur5_lego::Vectorize srv;
 	srv.request.x = x;
+	srv.request.dim = dim;
 
 	if (service_example.call(srv)) {
- 		ROS_INFO("Vector: (%ld,%ld)", (long int)srv.response.x, (long int)srv.response.y);
+		ROS_INFO("Vector: dim=%ld", (long int)srv.response.p.dim);
+		for(int i=0;i<srv.response.p.dim;++i){
+			ROS_INFO("  %d: (%ld,%ld)", i, (long int)srv.response.p.a.at(i).x, (long int)srv.response.p.a.at(i).y);
+		}
 	} else {
 		ROS_ERROR("Failed to call service vectorize_example");
 		return 1;
--- task_server_example.cpp
+++ task_server_example.cpp
@@ -1,12 +1,20 @@
 #include "ros/ros.h"
 #include "ur5_lego/Vectorize.h"
+#include <vector>
 
 bool vect(ur5_lego::Vectorize::Request &req, ur5_lego::Vectorize::Response &res)
 {
-	res.x = req.x;
-	res.y = req.x;
-	ROS_INFO("request: %ld", (long int)req.x);
-	ROS_INFO("sending back response: (%ld,%ld)", (long int)res.x, (long int)res.y);
+	res.p.dim = req.dim;
+	res.p.a.resize(req.dim);
+	for(int i=0;i<req.dim;++i){
+		res.p.a.at(i).x = req.x;
+		res.p.a.at(i).y = req.x;
+	}
+	ROS_INFO("request: x=%ld, dim=%ld", (long int)req.x, (long int)req.dim);
+	ROS_INFO("sending back response: dim=%ld", (long int)res.p.dim);
+	for(int i=0;i<req.dim;++i){
+		ROS_INFO("  %d: (%ld,%ld)", i, (long int)res.p.a.at(i).x, (long int)res.p.a.at(i).y);
+	}
 	return true;
 }
 
