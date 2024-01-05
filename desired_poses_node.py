e2c08c327ae323c4de50bc853b650921e7df40ad
Alex <wikialex02@gmail.com>
2024-01-05 12:27:42 +0100

    added desired_poses node and mudule

---
+++ desired_poses_node.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import rospy as ros
+
+from ur5_lego.srv import GetDesiredPoses,GetDesiredPosesRequest,GetDesiredPosesResponse
+from ur5_lego.msg import TargetPose
+
+from tf.transformations import quaternion_from_euler
+from ur5_lego_modules.desired_poses_params import desired_poses
+from ur5_lego_modules.desired_poses_params import z_offset
+
+
+info_name = "[desired_poses_node]:"
+debug_mode = True
+
+
+def get_desired_poses_handler(req):
+  ros.loginfo("%s desired_poses_node contacted", info_name)
+  res = GetDesiredPosesResponse()
+
+  res.dim = len(desired_poses)
+  for i in range(res.dim):
+    res.poses.append(TargetPose())
+
+    res.poses[i].label = desired_poses[i].label
+    res.poses[i].position.x = desired_poses[i].x
+    res.poses[i].position.y = desired_poses[i].y
+    res.poses[i].position.z = z_offset
+
+    res.poses[i].euler.x = 0.0
+    res.poses[i].euler.y = 0.0
+    res.poses[i].euler.z = desired_poses[i].theta
+
+    q = quaternion_from_euler(res.poses[i].euler.x, res.poses[i].euler.y, res.poses[i].euler.z, 'sxyz')
+    res.poses[i].orientation.x = q[0]
+    res.poses[i].orientation.y = q[1]
+    res.poses[i].orientation.z = q[2]
+    res.poses[i].orientation.w = q[3]
+
+
+  if debug_mode:
+    ros.loginfo("%s I have %d desired poses:", info_name, res.dim)
+    for i in range(res.dim):
+      pose = res.poses[i]
+      ros.loginfo("%s   %d: label=\"%s\", position=[%f,%f,%f], quaternion=%f+[%f,%f,%f], euler_angles=[%f,%f,%f]", info_name, i, pose.label, pose.position.x, pose.position.y, pose.position.z, pose.orientation.w, pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.euler.x, pose.euler.y, pose.euler.z)
+
+  return res
+
+
+if __name__ == "__main__":
+  ros.init_node("desired_poses_node")
+  service = ros.Service("get_desired_poses", GetDesiredPoses, get_desired_poses_handler)
+  ros.loginfo("%s desired_poses_node is ready!", info_name)
+  ros.spin()
--- task_planner.cpp
+++ task_planner.cpp
@@ -2,6 +2,7 @@
 
 #include "ur5_lego/GetBoundingBoxes.h"
 #include "ur5_lego/BoundingBoxesToPoses.h"
+#include "ur5_lego/GetDesiredPoses.h"
 #include "ur5_lego/MoveBlock.h"
 
 #include "ur5_lego/TargetPose.h"
@@ -22,30 +23,40 @@ int main(int argc, char **argv)
 
 	ros::init(argc, argv, "task_planner");
 	ros::NodeHandle node;
+
 	bool service_exit;
+	std::vector<ur5_lego::TargetPose> desired_poses;
+	std::vector<ur5_lego::TargetPose> actual_poses;
 
 	ros::ServiceClient vision_node = node.serviceClient<ur5_lego::GetBoundingBoxes>("get_bounding_boxes");
 	ros::ServiceClient pointcloud_node = node.serviceClient<ur5_lego::BoundingBoxesToPoses>("bounding_boxes_to_poses");
+	ros::ServiceClient desired_poses_node = node.serviceClient<ur5_lego::GetDesiredPoses>("get_desired_poses");
 	ros::ServiceClient motion_planner = node.serviceClient<ur5_lego::MoveBlock>("move_block");
 	vision_node.waitForExistence();
 	pointcloud_node.waitForExistence();
+	desired_poses_node.waitForExistence();
 	motion_planner.waitForExistence();
 
 	ur5_lego::GetBoundingBoxes get_bounding_boxes_service;
 	ur5_lego::BoundingBoxesToPoses bounding_boxes_to_poses_service;
+	ur5_lego::GetDesiredPoses get_desired_poses_service;
 	ur5_lego::MoveBlock move_block_service;
 
 	ROS_INFO("%s task_planner is ready!", info_name);
 
+
 	/// Vision Node
 
 	if(debug_mode) ROS_INFO("%s Vision phase", info_name);
+
+	// get_bounding_boxes_service.request is empty
 
 	service_exit = vision_node.call(get_bounding_boxes_service);
 	if(!service_exit){
 		ROS_ERROR("%s Failed to call service get_bounding_boxes", info_name);
 		return 1;
 	}
+
 
 	/// Pointcloud Node
 
@@ -60,25 +71,36 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	/// Retrieving Desired Poses
+	actual_poses = bounding_boxes_to_poses_service.response.poses;
+
+
+	/// Desired Poses Node
 
 	if(debug_mode) ROS_INFO("%s Retrieving desired poses", info_name);
 
-	std::vector<ur5_lego::TargetPose> desired_poses;
-	std::vector<ur5_lego::TargetPose> actual_poses = bounding_boxes_to_poses_service.response.poses;
+	// get_esired_poses_service.request is empty
 
-	desired_poses.push_back(ur5_lego::TargetPose());
-	actual_poses.push_back(ur5_lego::TargetPose());
-	desired_poses.at(0).label = "aaa";
-	actual_poses.at(0).label = "bbb";
-	desired_poses.push_back(ur5_lego::TargetPose());
-	actual_poses.push_back(ur5_lego::TargetPose());
-	desired_poses.at(1).label = "ccc";
-	actual_poses.at(1).label = "aaa";
+	service_exit = desired_poses_node.call(get_desired_poses_service);
+	if(!service_exit){
+		ROS_ERROR("%s Failed to call service get_desired_poses_service", info_name);
+		return 1;
+	}
+
+	desired_poses = get_desired_poses_service.response.poses;
+
 
 	/// Motion Planner
 
 	if(debug_mode) ROS_INFO("%s Motion planning phase", info_name);
+
+	/**/
+	actual_poses.push_back(ur5_lego::TargetPose());
+	actual_poses.push_back(ur5_lego::TargetPose());
+	actual_poses.push_back(ur5_lego::TargetPose());
+	actual_poses.at(0).label = "zzz";
+	actual_poses.at(1).label = "bbb";
+	actual_poses.at(2).label = "aaa";
+	/**/
 
 	ur5_lego::TargetPose actual_block;
 	for(ur5_lego::TargetPose desired_block : desired_poses){
@@ -108,6 +130,9 @@ int main(int argc, char **argv)
 	for(ur5_lego::TargetPose remaining_block : actual_poses){
 		ROS_WARN("%s Detected block \"%s\" with no desired_poses entry, it will be ignored", info_name, remaining_block.label.c_str());
 	}
+
+
+	/// End
 
 	ROS_INFO("%s task_planner is over!", info_name);
 	return 0;
